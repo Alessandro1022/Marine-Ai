63c1451e76fdd635551464acc34eff1c22caa814
Alessandro1022 <wehelie98@gmail.com>
2026-06-10 23:45:11 +0200

    Create LoadingScreen.tsx

---
+++ src/components/ui/LoadingScreen.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function LoadingScreen() {
+  return (
+    <div className="flex items-center justify-center py-16">
+      <div className="radar h-12 w-12" />
+    </div>
+  );
+}
