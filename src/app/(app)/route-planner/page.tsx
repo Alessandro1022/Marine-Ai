5778b8c0efce7baf3404d3ad05f3cf36fc41af35
Armand Wolfgang <wehelie98@gmail.com>
2026-08-27 09:42:09 +0200

    Update page.tsx

--- src/app/(app)/route-planner/page.tsx
+++ src/app/(app)/route-planner/page.tsx
@@ -45,7 +45,6 @@ export default function RoutePlannerPage() {
       fromLon: from.longitude,
       toLat: to.latitude,
       toLon: to.longitude,
-      boat: boat || null,
       fuelPriceSek: fuelPrice,
       weatherRisk: weather.risk,
     });
